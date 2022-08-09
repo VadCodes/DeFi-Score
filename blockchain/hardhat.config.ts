cb6a6da0c7deb2e44200149cb14c0ac1bbb64373
vincent.martel <vincent.martel@junifia.ca>
2022-08-09 14:53:07 -0400

    add hardhat tooling for hedera

---
+++ blockchain/hardhat.config.ts
@@ -0,0 +1,18 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: process.env.MNEMONIC
+        ? { mnemonic: process.env.MNEMONIC }
+        : [process.env.WALLET_PRIVATE_KEY!].filter(Boolean),
+      gas: 2100000,
+      gasPrice: 8000000000,
+    },
+  },
+};
+
+export default config;
