7e9c782b2b8792d0a85a4c017221734cd9a1fb98
vadim cote <vadim.cote@junifia.ca>
2022-08-09 17:52:18 -0400

    DeFiScore

--- hardhat/contracts/DefiScore.sol
+++ hardhat/contracts/DefiScore.sol
@@ -7,6 +7,7 @@ import "hardhat/console.sol";
 contract DefiScore {
     mapping(address => bool) public authorizedToRead;
     mapping(address => uint16) public creditScores;
+    mapping(address => address[]) public readCreditScores;
 
     // The minimum price to pay user for his credit score
     uint256 public price;
@@ -15,8 +16,16 @@ contract DefiScore {
     event GivePermission(address user);
     event PriceUpdated(uint256 price);
 
+    /**
+     * @notice Require that the sender is the dafounders DAO.
+     */
+    modifier minimumPrice() {
+        require(msg.value >= price, 'Must send at least minimum price');
+        _;
+    }
+
     constructor() {
-        // price = _price;
+         price = 1;
         // creditScores["address"] = 900;
         // creditScores["address"] = 600;
         // creditScores["address"] = 300;
@@ -27,9 +36,12 @@ contract DefiScore {
         emit GivePermission(msg.sender);
     }
 
-    function getCreditScore(address user) public {
+    function getCreditScore(address user) public payable minimumPrice returns (uint16) {
         require(authorizedToRead[user] == true, "User did not give permission to read his credit score");
-
+        
+        readCreditScores[user].push(msg.sender);
         emit ReadCreditScore(user, msg.sender);
+        payable(user).transfer(msg.value);
+        return creditScores[user];
     }
 }
