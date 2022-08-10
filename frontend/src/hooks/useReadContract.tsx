03b5da63a2abb3b71e098d1590084d6943880f3b
vadim cote <vadim.cote@junifia.ca>
2022-08-10 00:22:06 -0400

    opt-in opt-out

---
+++ frontend/src/hooks/useReadContract.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+
+import {
+    AccountId,
+    Client,
+    ContractFunctionParameters,
+    ContractCallQuery,
+    ContractFunctionResult
+} from '@hashgraph/sdk';
+
+const contractId = '0.0.47861487';
+
+const operatorId = process.env.REACT_APP_OPERATOR_ID;
+const operatorPK = process.env.REACT_APP_OPERATOR_PRIVATE_KEY;
+
+// If we weren't able to grab it, we should throw a new error
+if (operatorId == null || operatorPK == null) {
+  throw new Error(
+    "Environment variables myAccountId and myPrivateKey must be present"
+  );
+}
+
+const solidityAddress = AccountId.fromString(operatorId).toSolidityAddress();
+
+// Create our connection to the Hedera network
+// The Hedera JS SDK makes this really easy!
+const client = Client.forTestnet();
+
+client.setOperator(operatorId, operatorPK);
+
+export const useReadContract = () => {
+    const [result, setResult] = useState<ContractFunctionResult | null>(null);
+    
+    useEffect(() => {
+        const init = async () => {
+            try {
+                const response = await new ContractCallQuery()
+                    .setContractId(contractId)
+                    .setGas(25000)
+                    .setFunction('getReadCreditScores', new ContractFunctionParameters().addAddress(solidityAddress))
+                    .execute(client);
+                setResult(response);
+            } catch (err: any) {
+                console.log(err);
+            }
+        }
+        init();
+        
+    }, []);
+  return { result };
+};
---
+++ frontend/src/shared/OptOutButton.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+import {
+    AccountId,
+    Client,
+    ContractExecuteTransaction
+} from '@hashgraph/sdk';
+
+
+const contractId = process.env.REACT_APP_CONTRACT_ID;
+const operatorId = process.env.REACT_APP_OPERATOR_ID;
+const operatorPK = process.env.REACT_APP_OPERATOR_PRIVATE_KEY;
+
+// If we weren't able to grab it, we should throw a new error
+if (operatorId == null || operatorPK == null || contractId == null) {
+  throw new Error(
+    "Environment variables myAccountId and myPrivateKey must be present"
+  );
+}
+
+// Create our connection to the Hedera network
+// The Hedera JS SDK makes this really easy!
+const client = Client.forTestnet();
+
+client.setOperator(operatorId, operatorPK);
+
+const onSubmitOptOut = async () => {
+    try {
+        const contractExecuteTx = new ContractExecuteTransaction()
+            .setContractId(contractId)
+            .setGas(50000)
+            .setFunction("revokePermission")
+    
+            await contractExecuteTx.execute(client)
+    
+            alert("permission revoked !")
+        } catch (err: any) {
+            console.log(err)
+    }
+};
+
+export const OptOutButton = () => {
+    
+  return(<button
+  className="m-6 btn btn-blue"
+  onClick={() => onSubmitOptOut()}
+>
+  Revoke permission to read my credit score
+</button>)
+};
